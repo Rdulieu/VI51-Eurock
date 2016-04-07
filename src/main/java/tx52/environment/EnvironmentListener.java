921adcc0a6815bd4a6e720b252cadaec88876213
Olivier VILLEQUEY <olivier.villequey@utbm.fr>
2016-04-07 17:33:32 +0200

    Environment classes
    Creation of classes

---
+++ src/main/java/tx52/environment/EnvironmentListener.java
@@ -0,0 +1,8 @@
+package tx52.environment;
+
+import java.util.EventListener;
+
+
+public interface EnvironmentListener extends EventListener {
+	
+}
---
+++ src/main/java/tx52/environment/EnvironmentObject.java
@@ -0,0 +1,11 @@
+package tx52.environment;
+
+public class EnvironmentObject {
+	
+	public class Position {
+		float x;
+		float y;
+	}
+	
+	
+}
